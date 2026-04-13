cf4056c798a8659c7b94d929ba64306b1e2c37df
Javier Palomo <jpalomolaguna@gmail.com>
2026-04-13 10:15:36 +0200

    Fix: Sitemap con URLs directas para incluir servicios

--- ProyectoWebApp/sitemaps.py
+++ ProyectoWebApp/sitemaps.py
@@ -5,40 +5,29 @@ from blog.models import Post
 class StaticViewSitemap(Sitemap):
     priority = 0.8
     changefreq = 'weekly'
-    protocol = 'https'
-    
-    def get_urls(self, site=None, **kwargs):
-        site = type('Site', (), {
-            'domain': 'www.codigovivostudio.cloud'
-        })()
-        return super().get_urls(site=site, **kwargs)
 
     def items(self):
         return [
-            'index',
-            'servicios',
-            'tienda',
-            'contacto',
-            'blog',
+            {'url': '/', 'name': 'index'},
+            {'url': '/servicios/', 'name': 'servicios'},
+            {'url': '/tienda/', 'name': 'tienda'},
+            {'url': '/contacto/', 'name': 'contacto'},
+            {'url': '/blog/', 'name': 'blog'},
         ]
 
     def location(self, item):
-        return reverse(item)
+        return item['url']
 
 
 class BlogSitemap(Sitemap):
     priority = 0.7
     changefreq = 'monthly'
-    protocol = 'https'
-    
-    def get_urls(self, site=None, **kwargs):
-        site = type('Site', (), {
-            'domain': 'www.codigovivostudio.cloud'
-        })()
-        return super().get_urls(site=site, **kwargs)
 
     def items(self):
-        return Post.objects.all().order_by('-updated')
+        try:
+            return Post.objects.all()
+        except:
+            return []
 
     def lastmod(self, post):
         return post.updated
